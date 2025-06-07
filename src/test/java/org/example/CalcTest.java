ff2ae6b593f083f96642429ef9e13b84733d2164
neckapb <golodyuk@tut.by>
2025-06-07 05:04:09 +0300

    added first test

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -1,16 +1,26 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CalcTest {
+class CalcTest
+{
 
     @Test
-    void summ() {
+    void summ()
+    {
+        Calc calculator = new Calc();
+        int result = calculator.summ(6, 9);
+        Assertions.assertEquals(15, result, "Реальность отличается от ожиданий");
     }
 
     @Test
-    void difference() {
+    void difference()
+    {
+        Calc calculator = new Calc();
+        int result = calculator.difference(12, 3);
+        Assertions.assertEquals(9, result, "Реальность отличается от ожиданий");
     }
 }
